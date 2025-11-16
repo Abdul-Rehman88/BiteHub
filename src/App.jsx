3b63f4c9be615c6d7a896412c4d48a72017d6342
Abdul Rehman <abdulrehmanafridi88@gmail.com>
2025-11-16 16:14:21 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import { Button } from "@/components/base/buttons/button";
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Welcome to BiteHub!
+      </h1>
+      <Button>Click Me</Button>
+    </div>
+  )
+
+}
+
+export default App
---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyCEddJiL2uBgpFqnODAtNcgChES6p4hwm8",
+  authDomain: "bitehub-885a7.firebaseapp.com",
+  projectId: "bitehub-885a7",
+  storageBucket: "bitehub-885a7.firebasestorage.app",
+  messagingSenderId: "1044543460131",
+  appId: "1:1044543460131:web:a6747f57972c0b4447f296",
+  measurementId: "G-TNFRKPT6RQ"
+};
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+export default app;
